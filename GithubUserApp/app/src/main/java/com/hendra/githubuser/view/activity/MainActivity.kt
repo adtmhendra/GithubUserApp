1d90dfa691a193b8c317ee038323fa04e0fcf48b
Hendra Pratik Aditama <hendra.pratik09@gmail.com>
2020-08-19 19:57:21 +0700

    Add action bar menu

--- GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/MainActivity.kt
+++ GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.hendra.githubuser.view.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.hendra.githubuser.R
 
@@ -8,5 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.changeLanguage) {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+            return true
+        } else {
+            return true
+        }
     }
 }
