1091fe4cbc36dd5e9f5a28210fc7aa50b4399471
Hendra Pratik Aditama <hendra.pratik09@gmail.com>
2020-08-19 18:16:47 +0700

    Add image assets for this project

--- GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/SplashScreenActivity.kt
+++ GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/SplashScreenActivity.kt
@@ -2,11 +2,13 @@ package com.hendra.githubuser.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.hendra.githubuser.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
     }
 }
