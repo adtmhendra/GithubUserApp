ae59a02a9d9145ea9136fe428d604be339caa767
Hendra Pratik Aditama <hendra.pratik09@gmail.com>
2020-08-19 20:04:44 +0700

    Completing search feature

--- GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/MainActivity.kt
+++ GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.hendra.githubuser.view.activity
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.hendra.githubuser.R
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
+        searchView(menu)
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -26,5 +31,30 @@ class MainActivity : AppCompatActivity() {
         } else {
             return true
         }
+    }
+
+    private fun searchView(menu: Menu) {
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchableInfo = searchManager.getSearchableInfo(componentName)
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchableInfo)
+        searchView.queryHint = resources.getString(R.string.search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            // when search icon is pressed
+            override fun onQueryTextSubmit(query: String): Boolean {
+                showToast(this@MainActivity, query)
+                return true
+            }
+
+            // when text changed
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
