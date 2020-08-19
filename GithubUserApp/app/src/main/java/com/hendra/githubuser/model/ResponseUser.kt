6a81ab049e77b524e85a6200210b7672ba66e3ac
Hendra Pratik Aditama <hendra.pratik09@gmail.com>
2020-08-19 20:41:02 +0700

    Preparing data models

---
+++ GithubUserApp/app/src/main/java/com/hendra/githubuser/model/ResponseUser.kt
@@ -0,0 +1,15 @@
+package com.hendra.githubuser.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseUser(
+
+	@field:SerializedName("total_count")
+	val totalCount: Int? = null,
+
+	@field:SerializedName("incomplete_results")
+	val incompleteResults: Boolean? = null,
+
+	@field:SerializedName("items")
+	val items: List<ItemsItem?>? = null
+)
