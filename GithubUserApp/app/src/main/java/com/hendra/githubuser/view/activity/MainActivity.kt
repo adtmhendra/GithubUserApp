88501042c8873be4e7a8203756aab267971fe744
Hendra Pratik Aditama <hendra.pratik09@gmail.com>
2020-08-19 18:30:14 +0700

    Move from splash screen activity to main activity

--- GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/MainActivity.kt
+++ GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hendra.githubuser.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hendra.githubuser.R
 
 class MainActivity : AppCompatActivity() {
--- GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/SplashScreenActivity.kt
+++ GithubUserApp/app/src/main/java/com/hendra/githubuser/view/activity/SplashScreenActivity.kt
@@ -1,14 +1,37 @@
 package com.hendra.githubuser.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.hendra.githubuser.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
+
+        setAnimation()
+        moveToMainActivity()
+    }
+
+    private fun setAnimation() {
+        val splashScreenAnimation = AnimationUtils.loadAnimation(this, R.anim.splashscreen_anim)
+
+        imgSplashScreen.animation = splashScreenAnimation
+    }
+
+    private fun moveToMainActivity() {
+        GlobalScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
